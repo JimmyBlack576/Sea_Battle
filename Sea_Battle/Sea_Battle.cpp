b0e86ff66b24d57ca28e391b9145d50a8667f9b4
jimmyblackwood576@outlook.com <jimmyblackwood576@outlook.com>
2022-09-13 14:56:05 +0300

    Добавьте файлы проекта.

---
+++ Sea_Battle/Sea_Battle.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+void hand(int a) {
+    HANDLE h;
+    h = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(h, (((1 << 4) | 15)));
+}
+
+void hand1(int a) {
+    HANDLE h;
+    h = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(h, (((15 << 4) | 1)));
+}
+
+int main()
+{
+    HANDLE h;
+    h = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(h, (((15 << 4) | 1)));
+    //SetConsoleTextAttribute(h, 14);
+    //cout << "Hello World!\n";
+    for (int i = 0; i < 20; i++) {
+
+
+
+
+        for (int j = 0; j < 20; j++) {
+           // if (j < 1) {
+                //hand(j);
+              //  break;
+            //}
+            //else {
+              //  hand1(1);
+                cout << " 0 ";
+                Sleep(10);
+           // }
+        }
+        cout << endl;
+    }
+
+}
+
